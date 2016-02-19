0fa2724c9f0ba5aab40ec3d1f92cbe557e4a5883
TJay Cargle <tjcargle21@gmail.com>
2016-02-19 13:17:41 -0500

    Added files via upload

---
+++ YaSuMi/Assets/CirclePressurePlateController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+
+public class CirclePressurePlateController : MonoBehaviour
+{
+	public bool isPressed;
+	public string collisionTag;
+	public GameObject target;
+	private Transform tr;
+
+	// Use this for initialization
+	private void Start()
+	{
+		tr = GetComponent<Transform>();
+	}
+
+
+	public void targeting(GameObject obj)
+	{
+		target = obj;
+	}	
+
+	private void OnTriggerEnter(Collider _other)
+	{
+		if (_other.tag == collisionTag)
+		{
+			Press();
+		}
+	}
+
+	private void OnTriggerExit(Collider _other)
+	{
+		if (_other.tag == collisionTag)
+		{
+			UnPress();
+		}
+	}
+
+	private void Press()
+	{
+		isPressed = true;
+
+
+
+	}
+
+	private void UnPress()
+	{
+		isPressed = false;
+
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+	}
+}
